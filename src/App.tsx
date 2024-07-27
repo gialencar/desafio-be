b45cb3e70cb451ee28bee392f4073e0290a9b211
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-27 18:14:31 -0300

    feat: add tittle bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import searchIcon from "./assets/search.svg";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="flex justify-between py-8 max-sm:py-5 px-8 max-sm:flex-col max-sm:gap-7 ">
+        <h1>Funcionários</h1>
+        <div className="flex justify-end items-center relative">
+          <input
+            placeholder="Pesquisar"
+            className="border border-gray-10 rounded-lg px-4 py-3 w-72 max-sm:w-full"
+          />
+          <img
+            src={searchIcon}
+            className="absolute w-6 mr-4"
+            alt="Search Icon"
+          />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import svgImage from "../assets/logo-be.svg";
 
 export default function Header() {
   return (
-    <header className="px-8 max-sm:px-5 h-[60px] flex items-center">
+    <header className="px-8 max-sm:px-5 h-[60px] flex items-center shadow-3">
       <img src={svgImage} width={44} alt="BE logo" />
     </header>
   );
