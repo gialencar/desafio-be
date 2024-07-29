c5b1df9f1e3dbed2bc44ad183d14a04e18ab1a0f
Gilson Alencar <gilson.alencar94@gmail.com>
2024-07-29 19:50:54 -0300

    fix: update API URL to use Vite environment variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import { formatPhoneNumber } from "./utils/formatPhoneNumber";
 
 const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3000/employees";
+  process.env.VITE_API_URL || "http://localhost:3000/employees";
 
 type Employee = {
   id: string;
